2251a0354233e14a3fa0c7a72eea4a06f8c901e4
Tom <tompowellgames@gmail.com>
2019-04-08 00:22:54 +1200

    reupload of main page

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tom Powell</title>
+    <link rel="stylesheet" href="styles\styles.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/43d98386fa.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">WebDevTom</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-mobile" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-mobile">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><h4>Projects</h4> </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><h4>About</h4></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="./contact"><h4>Contact</h4></a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </header>
+      <main>
+        <!-- intro-->
+        <div class="intro">
+          <h1 id="name">Tom Powell</h1>
+          <h2 id="title">Web Developer</h2>
+          <h4 id="location">Auckland, New Zealand</h4>
+          <h4>SITE STILL UNDER CONSTRUCTION</h4>
+          <div class="icons">
+            <a href="https://twitter.com/WebDeveloperTom" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/tom-powell-046474152/" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+            <a href="https://github.com/webdevelopertom"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+          </div>
+        </div>
+
+        <!-- projects -->
+        </div>
+        <div class="project-list">
+          <div class="panel">Panel A</div>
+          <div class="panel">Panel B</div>
+          <div class="panel">Panel C</div>
+          <div class="panel">Panel D</div>
+          <div class="panel">Panel E</div>
+          <div class="panel">Panel F</div>
+        </div>
+        <div class="help">
+          <h2>Let me help you get in front of the world.</h2>
+          <p>We live in a digital age where everyone is connected online.</p>
+          <p>Don't let your business or idea get left behind and hidden.</p>
+          <p>Put it in the digital spotlight for everyone to see.</p>
+          <h2>With my experience I can: </h2>
+          <h4>Create a responsive, professional website that will work on a range of devices.</h4>
+          <h4>Set up social media accounts to ensure maximum exposure</h4>
+          <h4>And a range of other services</h4>
+        </div>
+
+      </main>
+      <footer>
+        <div class="footer-icons">
+          <a href="https://twitter.com/WebDeveloperTom" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/in/tom-powell-046474152/" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+          <a href="https://github.com/ThatLegenedTom"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <p>Tom Powell 2019</p>
+      </footer>
+  </body>
+</html>
