b282a2e8d8f7e9094b4254464e02b760ec81e699
WebDeveloperTom <tom@webdevelopertom.com>
2019-04-08 17:26:00 +1200

    add aboutme

---
+++ aboutme/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tom Powell</title>
+    <link rel="stylesheet" href="../styles\styles.css">
+    <link rel="stylesheet" href="../styles\aboutmestyles.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/43d98386fa.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">WebDevTom</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-mobile" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-mobile">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><h4>Projects</h4> </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><h4>About</h4></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="./contact"><h4>Contact</h4></a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <div class="aboutme-title">
+      <h3>About Tom</h3>
+      <h5>Top notch websites at an affordable price.</h5>
+    </div>
+      <main>
+
+        <div class="aboutme-main">
+          <div class="photo">
+            <img src="../assests\Small Tom Powell.jpg" alt="">
+          </div>
+
+          <p>I am a front-end web developer with over 12 years in customer service.
+            I specialise in creating simple but beautiful and responsive front-end expierences.</p>
+          
+            <p>I have been studying web development for the past 3 years and have become profient in:</p>
+            <ul>
+              <li>Responsive HTML/CSS</li>
+              <li>JavaScript (vanilla, jQuery, React)</li>
+              <li>High-level user experience</li>
+              <li>Professional best practices</li>
+              <li>Git</li>
+              <li>WordPress</li>
+            </ul>
+            <br>
+            <p>Outside of programing, I help my partner run her website, AttackOnGeek.com.</p>
+            <p>You'll also find me streaming games to our community on a regular basis over at Mixer.com/AttackOnGeek.</p>
+        </div>
+
+
+
+      </main>
+      <footer>
+        <div class="footer-icons">
+          <a href="https://twitter.com/WebDeveloperTom" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/in/tom-powell-046474152/" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+          <a href="https://github.com/ThatLegenedTom"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <p>Tom Powell 2019</p>
+      </footer>
+  </body>
+</html>
